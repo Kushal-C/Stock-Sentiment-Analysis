ea9c651c0cb1d1bd5bcd32fdd187e9942956b243
Kushal Cuttari <kcutari@gmail.com>
2018-12-26 14:24:53 -0800

    Initial Setup

---
+++ stock-sentiment-analyzer/src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../css/styles.css";
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="header-accent">Stock Sentiment Analysis</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ stock-sentiment-analyzer/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home';
+import "./css/styles.css";
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
